7fd9bcead70073d05540337f1a5f3bba68a2c2fa
Adanedhel <furkan.peon@gmail.com>
2023-05-03 13:50:07 +0300

    Clean Code Giris

---
+++ src/com/adanedhel/hafta08/cleanCode/CleanCodeOrnekler.java
@@ -0,0 +1,53 @@
+package com.adanedhel.hafta08.cleanCode;
+
+public class CleanCodeOrnekler {
+	/*
+	 * Clean CODE : Tekrarsız ve Anlaşılabilirdir. Açıklamalı. İsimlendirmeler doğru yapılmalıdır. Geliştirilmeye açık olmalı
+	 * Fonsiyonel olarak ayrı ayrı yazılmalıdır. Tekrar tekrar kullanılabilecek kodlar olmalı. Camel Case gibi isimlendirme yapıları kullanılmalı.
+	 * Access Modifierların kullanımına dikkat edilmeli
+	 * Hiyerarşik package yapısı kurulmalı
+	 * 
+	 * Temiz Kod: Bu kurallara uyulması zorunlı değildir. Uymadan da bu çalışan kodlar yazabilirsiniz. Burada verilen kurallar iyi programcıların önerileridir.
+	 * 
+	 * Temiz koda nasıl ulaşılır?
+	 * Karmakarışık kodu iyileştirerek ulaşılır
+	 * 
+	 * 4 Temel Ana Kural:
+	 * Basit Olmalı --> Anlaşılır > Standartlara uygun olmalı., Kısa.
+	 * Odaklı Olmalı --> Tekrarsız, Tek Hedefli(İlgili her şey, ilgisiz hiçbir şey)
+	 * Dogru Olmalı --> Kod doğrulanmış olmalı. Entegre edildiğinde uyumlu olmalı.
+	 * 
+	 * Anlaşılır:
+	 * Standartlar -> Isimlendirme Standartlari
+	 * Package Isımlendirme:			tr.com.hak5 //URL TERSİ
+	 * Sınıf Isimlendirme: 				ClassName
+	 * Metod Isımlendirme: 				metodName
+	 * Degisken Isımlendirme: 			degiskenIsimlendirme
+	 * final Degisken Isımlendirme: 	DUNYANIN_CAPI
+	 * 
+	 * Girintiler:
+	 * Dökümantasyon: Yazdiginiz tum metodlara aciklama ekleyin
+	 * 
+	 * Sınıflarda kaç tane metod olmalı?
+	 * Bir sınıf içinde genelde 20den fazla metod olmamalı 
+	 * 
+	 * Bir metod kaç satır olmalı?
+	 * Genelde 20 satırdan fazla olmamalı
+	 * 
+	 * Metod içinde kaç tane değişken olmalı?
+	 * Genelde 3 4 değişken yeterli olmalıdır
+	 * 
+	 * Bir metodun yaptığı işi başka bir metodun gövdesinde görürsek?
+	 * Copy paste --> mümkün olduğu kadar kod copy paste yapmayın.
+	 * Cut pasta yapın. Yeni bir metod yapıştır
+	 * 
+	 * Kodlarınızı her zaman düzenli yazın.
+	 * Timeline'a yada Deadline'a yetişeceğim diye kurallara uymadan yazarsan ilerde kodları toparlamak için çok daha fazla zaman harcarsın.
+	 * 
+	 * Metodlar içinde karar yapısı. if else if blok sayısı kaç olabilir? if-else switch
+	 * 10'dan az olmalı.
+	 * 
+	 * Thread uygulamalarında aynı java dosyası içinde birden fazla class kullandık. 
+	 * 
+	 */
+}
